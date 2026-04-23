50565bcf59d0f339fe19f77493d554be5a224f0e
amor <amorsaralynn@gmail.com>
2026-04-23 20:57:37 +0300

    added an image for internation.html page

--- hero.js
+++ hero.js
@@ -38,3 +38,59 @@ document.addEventListener("DOMContentLoaded", () => {
     const welcome = new Welcomescreen("welcome-display");
     welcome.displayonscreen();
 });
+
+
+
+
+
+const viewscholarship=document.getElementById('viewscholarships');
+const expandedbox = {
+viewscholarship: viewscholarship,
+}
+
+
+
+const viewScholarshipBtn = document.getElementById('viewscholarships');
+const overlay = document.getElementById('overlay');
+const popup = document.getElementById('popup');
+
+// OPEN
+function openpathbox() {
+  overlay.classList.remove('hidden');
+  overlay.classList.add('flex');
+
+  popup.classList.remove('scale-95', 'opacity-0');
+  popup.classList.add('scale-100', 'opacity-100');
+}
+
+// CLOSE
+function closecontent() {
+  popup.classList.add('scale-95', 'opacity-0');
+
+  setTimeout(() => {
+    overlay.classList.add('hidden');
+    overlay.classList.remove('flex');
+  }, 200);
+}
+
+// click button
+viewScholarshipBtn.addEventListener('click', openpathbox);
+
+// click outside
+overlay.addEventListener('click', function (e) {
+  if (e.target === overlay) {
+    closecontent();
+  }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const storedUser = JSON.parse(localStorage.getItem("userlogindata"));
+
+  const name = storedUser?.username || "there";
+
+  document.getElementById("userName").textContent = name;
+
+});
