1b50197511dd6ed8bed8e31b981662ccc0a29797
amor <amorsaralynn@gmail.com>
2026-04-27 11:50:49 +0300

    added log out.html file to handle log out events

---
+++ signup.js
@@ -0,0 +1,9 @@
+
+
+
+const signUpButton = document.getElementById("sign-up");
+
+signUpButton.addEventListener("click", function (e) {
+    e.preventDefault(); 
+    window.location.replace("hero.html");
+});
